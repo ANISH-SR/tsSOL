f07db5ccc6975fdd6e878d4bebc0488d7d1555e1
ANISH-SR <rawatanish08@gmail.com>
2025-03-29 22:37:50 +0530

    Changed confirmTransaction derecated fn

--- airdrop/index.ts
+++ airdrop/index.ts
@@ -5,6 +5,17 @@ export const airdrop = async (address: PublicKey, amount: number) => {
     const conn = new Connection("https://api.devnet.solana.com", "confirmed");
     
     const signature = await conn.requestAirdrop(publicKey, amount* LAMPORTS_PER_SOL);
-    await conn.confirmTransaction(signature);
+    // await conn.confirmTransaction(signature);
     //Change confirmTransaction  -> DEPRECATED
+
+    const latestBlockhash = await conn.getLatestBlockhash();
+    const confirmStrategy = {
+        blockhash: latestBlockhash.blockhash,
+        lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+        signature: signature
+    }
+
+    await conn.confirmTransaction(confirmStrategy, "confirmed");
 }
+
+// airdrop("4Bt7w7N4ZC796UkhYhzbx9Z6x8mntXeco2tJCnwCDJd1", 5)
